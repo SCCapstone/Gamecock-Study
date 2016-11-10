f00540740230155fef3c1ba07c4b91fa88a5ba46
JamesEverette <jameseds95@gmail.com>
2016-11-10 15:59:35 -0500

    Added more methods for authentication.

--- app/src/main/java/notificationClass.java
+++ app/src/main/java/notificationClass.java
@@ -1,17 +1,20 @@
+import android.util.Log;
+
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
 
 /**
  * Created by shivangip on 11/10/16.
  */
 
-public class notificationClass {
+public class notificationClass extends FirebaseInstanceIdService{
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        //Log.d(TAG, "Refreshed token: " + refreshedToken);
 
         // TODO: Implement this method to send any registration to your app's servers.
-        sendRegistrationToServer(refreshedToken);
+        //sendRegistrationToServer(refreshedToken);
     }
 }
