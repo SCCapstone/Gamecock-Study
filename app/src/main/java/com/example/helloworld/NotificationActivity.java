0835614aca2a749b4b40e5d6ed2fe254df82840f
patshiv <shivangip951@gmail.com>
2016-11-23 16:15:16 -0500

    Alllows push notification

--- app/src/main/java/com/example/helloworld/NotificationActivity.java
+++ app/src/main/java/com/example/helloworld/NotificationActivity.java
@@ -1,15 +1,34 @@
 package com.example.helloworld;
 
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.app.NotificationManager;
+
+
 /**
  * Created by shivangip on 11/10/16.
  */
 
-public class notificationClass extends FirebaseInstanceIdService{
+
+
+public class NotificationActivity extends FirebaseInstanceIdService{
+
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 33;
+
+
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
@@ -19,4 +38,6 @@ public class notificationClass extends FirebaseInstanceIdService{
         // TODO: Implement this method to send any registration to your app's servers.
         //sendRegistrationToServer(refreshedToken);
     }
+
+
 }
---
+++ app/src/main/java/com/example/helloworld/PushActivity.java
@@ -0,0 +1,47 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.app.NotificationManager;
+
+
+public class PushActivity extends AppCompatActivity {
+
+
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 33;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push);
+
+        notification=new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+    }
+    public void NotificationClicked(View view){
+        //build notification
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker("This is the ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Here is the title");
+        notification.setContentText("I am the body text of the notification");
+
+        Intent intent=new Intent(this,PushActivity.class);
+        //gives access to intents in our app
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Builds notification and issues it (issuing=sending out to device)
+        NotificationManager nm=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID,notification.build());
+    }
+}
