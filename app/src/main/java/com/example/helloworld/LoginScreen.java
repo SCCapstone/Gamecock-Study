4cd553f486abf499f07578999dd90f32288c69bc
moralerr <moralerr@email.sc.edu>
2016-11-23 19:05:56 -0500

    create and login button now switch to Main2Activity screen

--- app/src/main/java/com/example/helloworld/LoginScreen.java
+++ app/src/main/java/com/example/helloworld/LoginScreen.java
@@ -181,4 +181,16 @@ public class LoginScreen extends AppCompatActivity implements View.OnClickListen
             signIn(emailField.getText().toString(), passwordField.getText().toString());
         }
     }
+
+    public void signInClicked(View view)
+    {
+        Intent intent = new Intent(LoginScreen.this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void createButtonClicked(View view)
+    {
+        Intent intent = new Intent(LoginScreen.this, Main2Activity.class);
+        startActivity(intent);
+    }
 }
