4c422587c59d9bd41d0d33fdb023cb9d294ce57c
susanacuellar <scuellar@email.sc.edu>
2016-11-27 11:15:45 -0500

    modified the SearchScreen class. moved the search box to the top of the page

--- app/src/main/java/com/example/helloworld/SearchScreen.java
+++ app/src/main/java/com/example/helloworld/SearchScreen.java
@@ -22,6 +22,7 @@ public class SearchScreen extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
     }
