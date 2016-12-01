78b20749282fee1c6a67d5e9f235ea4e7285f97d
JamesEverette <jameseds95@gmail.com>
2016-12-01 17:04:03 -0500

    Revert "Revert "reset back to suzy's commit""
    This reverts commit ef77453c46533662fec060041a06e5362ffa2da9.

--- app/src/main/java/com/example/helloworld/CreateGroup.java
+++ app/src/main/java/com/example/helloworld/CreateGroup.java
@@ -23,6 +23,7 @@ public class CreateGroup extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //hey
             }
         });
     }
