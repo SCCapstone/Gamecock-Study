5797b7bc093b6fc69cf9b516cd2792519780795b
JamesEverette <jameseds95@gmail.com>
2017-03-14 00:44:23 -0400

    added nav drawer to create study event page

--- app/src/main/java/edu/sc/cse/CreateGroup.java
+++ app/src/main/java/edu/sc/cse/CreateGroup.java
@@ -1,14 +1,22 @@
 package edu.sc.cse;
 import edu.sc.cse.R;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Context;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.RemoteViews;
 import android.media.RingtoneManager;
 import android.media.Ringtone;
@@ -47,7 +55,7 @@ import java.util.Calendar;
 import java.util.List;
 
 
-public class CreateGroup extends AppCompatActivity implements View.OnClickListener
+public class CreateGroup extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener
 {
 
     private static final String TAG = "MainActivity";
@@ -80,6 +88,17 @@ public class CreateGroup extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_create_group);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         context =this;
         notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -251,5 +270,64 @@ public class CreateGroup extends AppCompatActivity implements View.OnClickListen
 
 
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_findEvent) {
+            startActivity(new Intent(CreateGroup.this, SearchScreen.class));
+            // Handle the camera action
+        } else if (id == R.id.nav_createEvent) {
+            startActivity(new Intent(CreateGroup.this, CreateGroup.class));
+
+        } else if (id == R.id.nav_myEvents) {
+            startActivity(new Intent(CreateGroup.this, MyEvents.class));
+
+        } else if (id == R.id.nav_logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(CreateGroup.this, LoginScreen.class));
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
 
