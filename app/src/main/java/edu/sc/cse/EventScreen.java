d53b390edbdcaf41db7a2ba3832ffc7964771fb3
moralerr <moralerr@email.sc.edu>
2017-04-06 17:54:54 -0400

    Change EditText to TextView

--- app/src/main/java/edu/sc/cse/EventScreen.java
+++ app/src/main/java/edu/sc/cse/EventScreen.java
@@ -7,28 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class EventScreen extends AppCompatActivity {
-    private EditText course;
-    private EditText event_date;
-    private EditText event_time;
-    private EditText event_location;
-    private EditText event_host;
-    private EditText event_members;
-    private EditText event_description;
+    private TextView course;
+    private TextView event_date;
+    private TextView event_time;
+    private TextView event_location;
+    private TextView event_host;
+    private TextView event_members;
+    private TextView event_description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        course = (EditText) findViewById(R.id.course);
-        event_date = (EditText) findViewById(R.id.event_date);
-        event_time = (EditText) findViewById(R.id.event_time);
-        event_location = (EditText) findViewById(R.id.event_location);
-        event_host = (EditText) findViewById(R.id.event_host);
-        event_members = (EditText) findViewById(R.id.event_members);
-        event_description = (EditText) findViewById(R.id.event_description);
+        course = (TextView) findViewById(R.id.course);
+        event_date = (TextView) findViewById(R.id.event_date);
+        event_time = (TextView) findViewById(R.id.event_time);
+        event_location = (TextView) findViewById(R.id.event_location);
+        event_host = (TextView) findViewById(R.id.event_host);
+        event_members = (TextView) findViewById(R.id.event_members);
+        event_description = (TextView) findViewById(R.id.event_description);
         course.setText(MyEvents.currentG.getCourse());
         event_date.setText(MyEvents.currentG.getDate());
         event_time.setText(MyEvents.currentG.getTime());
